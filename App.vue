66f3110e1eed1521d88b7bacf55c74d4bc06fa94
anki356 <39798400+anki356@users.noreply.github.com>
2021-06-25 21:21:35 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    
+  <todo></todo>
+  </div>
+</template>
+
+<script>
+/*import HelloWorld from './components/HelloWorld'*/
+import todo from './components/todo'
+
+export default {
+  name: 'App',
+  components: {
+    todo
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;}
+
+  .container{
+    max-width: 600px;
+    margin : @ auto;
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div >
+      <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+      <div v-for="todo in todos" :key="todo.id" class="todo-item">
+          {{todo.title}}
+          </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+        todos:[
+            { newTodo: '',
+             'id':1,
+              'title':'',
+              'completed':false,
+          },
+        ]
+    }
+  },
+          methods : {
+          addTodo(){
+            this.todos.push({
+                id: this.id,
+                title: this.newTodo,
+                completed:false,
+            })
+          
+      
+
+this.newTodo=" "
+this.id++
+          }
+          }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+</style>
